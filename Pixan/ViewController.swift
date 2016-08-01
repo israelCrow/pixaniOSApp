453aea4670173ec8a0e92d58731531d9d1370c98
Alejandro Aristi C <pequenocuervo@gmail.com>
2016-08-01 10:36:06 -0500

    Initial Commit

---
+++ Pixan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pixan
+//
+//  Created by Alejandro Aristi C on 8/1/16.
+//  Copyright © 2016 Alejandro Aristi C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ PixanUITests/PixanUITests.swift
@@ -0,0 +1,36 @@
+//
+//  PixanUITests.swift
+//  PixanUITests
+//
+//  Created by Alejandro Aristi C on 8/1/16.
+//  Copyright © 2016 Alejandro Aristi C. All rights reserved.
+//
+
+import XCTest
+
+class PixanUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
